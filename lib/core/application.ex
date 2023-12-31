e9e9f3d0edb4f51fc4437dd724a94ff110a4d12d
farhansyedain <farhansyedain@gmail.com>
2023-12-31 22:15:02 +0530

    Use proper module name

--- lib/core/application.ex
+++ lib/core/application.ex
@@ -1,11 +1,11 @@
-defmodule SecureChatBackend.Application do
+defmodule Core.Application do
   use Application
 
   @impl true
   def start(_type, _args) do
     children = [
       {Core.Repo, []},
-      {Plug.Cowboy, scheme: :http, plug: Core.EntryPlug, options: [port: 8080]}
+      {Plug.Cowboy, scheme: :http, plug: Core.EntryPlug, options: [port: 8000]}
     ]
 
     opts = [strategy: :one_for_one, name: SecureChatBackend.Supervisor]
