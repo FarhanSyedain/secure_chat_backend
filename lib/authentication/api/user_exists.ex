08697b5afc98dca1d54a6e4f6aa9b24af265166e
Farhan Syedain <farhansyedain@gmail.com>
2025-05-02 18:12:54 +0530

    Add field has_pin

--- lib/authentication/api/user_exists.ex
+++ lib/authentication/api/user_exists.ex
@@ -7,10 +7,10 @@ defmodule Authentication.Api.UserExists do
     with %{"phone_number" => phone_number} <- conn.params do
       case Users.user_exists?(phone_number) do
         true ->
-          user_locked? = is_user_locked?(phone_number)
+
           conn
           |> put_resp_content_type("application/json")
-          |> send_resp(200, Jason.encode!(%{code: "user_exists", message: "User exists",data: %{is_user_locked: user_locked?}}))
+          |> send_resp(200, Jason.encode!(%{code: "user_exists", message: "User exists",data: %{is_user_locked: is_user_locked?(phone_number),has_pin_verifications: has_pin_verifications?(phone_number)}}))
 
         false ->
           conn
@@ -25,8 +25,15 @@ defmodule Authentication.Api.UserExists do
     end
   end
 
-  
+  defp has_pin_verifications?(phone_number) do
+    Users.has_pin_verifications?(phone_number)
+  end
+
   defp is_user_locked?(phone_number) do
+
+    Users.has_pin_verifications?(phone_number)
+    |> case do
+      true ->
     NaiveDateTime.compare(
       NaiveDateTime.truncate(
         NaiveDateTime.add(Users.get_user(phone_number),7,:day), :second
@@ -35,6 +42,8 @@ defmodule Authentication.Api.UserExists do
   NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
 
     ) == :gt
-
+    false ->
+      false
+  end
   end
 end
